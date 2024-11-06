277587c64105029ccc0af32484442d10d3f680f1
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-11-06 13:08:15 +0530

    create kiya footer or design kiya

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
+      <Footer/>
       </BrowserRouter>
     </>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <>
+      {" "}
+      <footer className="section__container footer__container">
+        <div className="footer__col">
+          <h4>CONTACT INFO</h4>
+          <p>
+            <span>
+              <i className="ri-map-pin-2-fill"></i>
+            </span>
+            123, India Aroma Circle, India
+          </p>
 
-export default Footer
+          <p>
+            <span>
+              <i className="ri-mail-fill"></i>
+            </span>
+            krunal@gmail.com
+          </p>
+
+          <p>
+            <span>
+              <i className="ri-phone-fill"></i>
+            </span>
+            +91 7435045502
+          </p>
+        </div>
+
+        <div className="footer__col">
+          <h4>COMPANY</h4>
+          <Link to="/">Home</Link>
+          <Link to="/">About Us</Link>
+          <Link to="/">Work With Us</Link>
+          <Link to="/">Our Blogs</Link>
+          <Link to="/">Term & Conditions</Link>
+        </div>
+
+        <div className="footer__col">
+          <h4>USEFULL LINKS</h4>
+          <Link to="/">Help</Link>
+          <Link to="/">Track Your Orders</Link>
+          <Link to="/">Men</Link>
+          <Link to="/">Women</Link>
+          <Link to="/">Dresses</Link>
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <h4>Social Media Handle</h4>
+          <div className="flex gap-5">
+            <a href="" className="text-3xl hover:text-red-500">
+              <i class="ri-instagram-line"></i>
+            </a>
+            <a href="" className="text-3xl hover:text-red-500">
+              <i class="ri-facebook-circle-line"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+      <div className="footer__bar">
+        Copyright © 2025 All rights reserved By Let's Shop
+      </div>
+    </>
+  );
+};
+
+export default Footer;
