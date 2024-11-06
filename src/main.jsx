183d8dc3f4ac57df1aed69247bc363bfb81b15db
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-11-06 18:20:06 +0530

    redux toolkit setup kiya

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'remixicon/fonts/remixicon.css'
+import { Provider } from 'react-redux'
+import { store } from './Redux/store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
